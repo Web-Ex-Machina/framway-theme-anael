3659a586580ea04f8e51cb763a77e563b4e58699
SeptimusVII <vansteene.quentin@gmail.com>
2026-07-07 10:48:38 +0200

    add style, js, config

--- config.js
+++ config.js
@@ -1,1 +1,43 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'blue'  : '#0070c0',
+		'blue_1': '#0d2bff', 
+		'blue_2': '#1451d0',
+		'blue_3': '#1288f6',
+		'blue_4': '#0ec9ff',
+	},
+	'primary': 'colors(blue_1)',
+
+	'radius':'25px',
+
+	'footer': {
+		'background': 'transparent',
+	},
+	'btn':{
+		'radius': '100px'
+	},
+
+	'input': {
+		'background': 'colors(white)',
+		'font-color': 'colors(greystronger)',
+		'border-color': 'primary',
+		'border-size': 'border-default-size',
+		'placeholder-font-color': 'colors(grey)',
+		'radius': true,
+	},
+	'input-focus': {
+		// 'background': 'input(background)',
+		// 'font-color': 'input(font-color)',
+		'border-color': 'input(border-color)',
+	},
+	// 'input-valid': {
+	// 	'background': 'input(background)',
+	// 	'font-color': 'input(font-color)',
+	// 	'border-color': 'input(border-color)',
+	// },
+	'input-invalid': {
+		'background': 'input(background)',
+		'font-color': 'input(font-color)',
+		'border-color': 'error',
+	},
+};
